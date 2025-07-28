b4eee8c257b653a9e877bb735d8e4d696a596844
jhersy143 <jhersyafernandezit@gmail.com>
2025-07-29 02:10:56 +0800

    signup started

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,23 +5,28 @@ import { ReactNode } from "react";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-auth-light bg-cover bg-center bg-no-repeat px-4 py-10 dark:bg-auth-dark">
-      <section className="light-border background-light800_dark200 shadow-light100_dark100 min-w-full rounded-[10px] border px-4 py-10 shadow-md sm:min-w-[520px] sm:px-8">
-        <div className="flex items-center justify-between gap-2">
-          <div className="space-y-2.5">
-            <h1 className="h2-bold text-dark100_light900">Join DevFlow</h1>
-            <p className="paragraph-regular text-dark500_light400">
-              To get your questions answered
-            </p>
-          </div>
-          <Image
-            src="images/site-logo.svg"
+    <main className=" flex-row min-h-screen items-center justify-center bg-auth-light bg-cover bg-center bg-no-repeat px-4 py-10 dark:bg-auth-dark justify-items-center">
+      <section >
+        <div className="flex items-center justify-center mb-8 space-x-2">
+              <Image
+            src="/images/logopsu.png"
             alt="DevFlow Logo"
-            width={50}
-            height={50}
+            width={100}
+            height={100}
             className="object-contain"
           />
+          <div className="space-y-1">
+            <h1 className="h1-bold text-dark100_light900 text-2xl">PANGASINAN STATE UNIVERSITY</h1>
+           
+          </div>
+          
         </div>
+        <p className="font-bold text-dark500_light400 mb-8 text text-lg">
+              STUDENT PERFORMANCE TRACKING SYSTEM FOR COMPUTER SCIENCE STUDENTS OF PSULC
+            </p>
+      </section>
+      <section className="light-border background-light800_dark200 shadow-light100_dark100 min-w-full rounded-[10px] border px-4 py-10 shadow-md sm:min-w-[400px] sm:px-8 w-[500px] self-center">
+        
 
         {children}
 
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,3 +1,20 @@
-"use client"
+"use client";
 
 import React from "react";
+
+import AuthForm from "../shared/AuthForm";
+import { signInWithCredentials } from "../actions/auth.actions";
+import { SignUpSchema } from "@/lib/validations";
+
+const SignIn = () => {
+  return (
+    <AuthForm
+      formType="SIGN_IN"
+      schema={SignUpSchema}
+      defaultValues={{ username: "", password: "" }}
+      onSubmit={signInWithCredentials}
+    />
+  );
+};
+
+export default SignIn;
